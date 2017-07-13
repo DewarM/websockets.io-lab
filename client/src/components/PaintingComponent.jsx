dbb1f5f37e2a6b65a897b8847dc9b8aeac7a15b6
Chris Woodall <christopherhwoodall@gmail.com>
2017-07-13 12:04:58 +0100

    more init

---
+++ client/src/components/PaintingComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class PaintingComponent extends React.Component {
+
+
+
+
+  render() {
+    return(
+      <div>
+        Another labs 
+
+      </div>
+      )
+  }
+
+}
+
+export default PaintingComponent
--- client/src/containers/PaintingContainer.jsx
+++ client/src/containers/PaintingContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PaintComponent from '../components/PaintingComponent'
 
 
 class PaintingContainer extends React.Component {
@@ -9,7 +10,8 @@ class PaintingContainer extends React.Component {
   render() {
     return(
       <div>
-        Hello World
+        Hello World!
+        <PaintComponent/>
 
       </div>
       )
